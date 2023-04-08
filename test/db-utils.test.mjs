dde0d262b63b60b00e5c7bc35dd69e1686e1dc27
franok <dev@franok.de>
2023-04-08 22:56:52 +0200

    setup before test config

---
+++ test/db-utils.test.mjs
@@ -0,0 +1,12 @@
+import * as dbUtils from '../util/db-utils.mjs';
+
+// Todo: implement
+describe('db utils', () => {
+    it('getSoftwareJson',() => {
+        // dbUtils.getSoftwareJson();
+    });
+
+    it('updateSoftwareUpdates',() => {
+        // dbUtils.updateSoftwareUpdates(null);
+    });
+});
