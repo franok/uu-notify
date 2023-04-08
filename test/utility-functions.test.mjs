a3269275816f9db866f4372ec2b9628cd0f0de90
franok <dev@franok.de>
2023-04-08 22:37:19 +0200

    prepare further tests

---
+++ test/utility-functions.test.mjs
@@ -0,0 +1,20 @@
+import * as utilityFunctions from '../util/octokit-utils.mjs';
+
+// Todo: implement
+describe('utility functions', () => {
+    it('compareLastInfosWithCurrentSoftwareDepsList',() => {
+        // utilityFunctions.compareLastInfosWithCurrentSoftwareDepsList(null, null);
+    });
+
+    it('isUpdateAvailable',() => {
+        // utilityFunctions.isUpdateAvailable(null);
+    });
+
+    it('prepareDataForNotification',() => {
+        // utilityFunctions.prepareDataForNotification(null);
+    });
+
+    it('updateData',() => {
+        // utilityFunctions.updateData(null);
+    });
+});
