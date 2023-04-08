f9c096d8ff0199f5b0b8c471e7dc25b24b18e120
franok <dev@franok.de>
2023-04-08 19:18:57 +0200

    test config and swDeps

--- test/config.test.mjs
+++ test/config.test.mjs
@@ -13,9 +13,6 @@ before(() => {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     fs.copyFileSync(path.resolve(__dirname, "../config/config.json.template"), path.resolve(__dirname, "test-config.json"));
     config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "test-config.json")));
-
-    // fs.copyFileSync(path.resolve(__dirname, "../config/software-deps.mjs.example"), path.resolve(__dirname, "test-software-deps.mjs"));
-
 });
 
 
@@ -31,12 +28,17 @@ describe('Config', () => {
     });
 });
 
-// test('softwareDeps are valid', () => {
-//     expect(swDeps).toBeDefined();
-//     swDeps.forEach(dep => {
-//         expect(dep).toHaveProperty('name');
-//     });
-// });
+describe('SoftwareDeps', () => {
+    it('are valid', () => {
+        swDeps.forEach(dep => {
+            expect(dep).to.have.property('name');
+            expect(dep).to.have.property('feedUrl');
+            expect(dep).to.have.property('github');
+            expect(dep.github).to.have.property('org');
+            expect(dep.github).to.have.property('repo');
+        });
+    });
+});
 
 after(() => {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
